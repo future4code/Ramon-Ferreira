a1567b9b65e5f6a2bedccc6a1dab0b064f8f87e9
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-07-19 16:41:31 -0300

    formulario

---
+++ semana6/formulario/formulario/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+
+export default class App extends React.Component {
+  state = {
+    value: ''
+  }
+render () {
+  return (
+    <div className="App">
+      <button>Próxima etapa</button>
+    </div>
+  );
+}
+}
+export default App;
---
+++ semana6/formulario/formulario/src/components/Final.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+    return (
+      <div>
+        <header>O formulário acabou.</header>
+        <p>Muito obrigado por participar! Entraremos em contato!</p>
+      </div>
+    );
+  }
+  
+  export default App;
