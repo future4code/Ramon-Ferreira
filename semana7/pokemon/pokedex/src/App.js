60c758d681dfafef7fc3fb83dc7617c13817b954
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-08-09 16:55:52 -0300

    Hooks-Introdução

--- semana7/pokemon/pokedex/src/App.js
+++ semana7/pokemon/pokedex/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const BorderTop = styled.div`
+  border-top: 3.5px solid var(--main-buttons-color);
+  position: absolute;
+  top: -3px;
+  left: 20px;
+  right: 19.5px;
+`;
+
+const BorderBottom = styled.div`
+  border-top: 3.5px solid var(--main-buttons-color);
+  position: absolute;
+  top: 11px;
+  left: 20px;
+  right: 19.5px;
+`;
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div className="nav">
+        <h1>Pokedex</h1>
+        <div className="nav-center-circle"></div>
+        <div className="nav-button-vertical"></div>
+        <div className="nav-button-horizontal">
+          <BorderTop />
+          <BorderBottom />
+        </div>
+      </div>
+    );
+  }
 }
-
-export default App;
