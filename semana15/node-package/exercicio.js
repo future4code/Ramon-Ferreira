71277d5113194665eb1ea18f79739f7c5cc18101
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-09-21 19:27:01 -0300

    exercicio

---
+++ semana15/node-package/exercicio.js
@@ -0,0 +1,46 @@
+// Exercicio 1
+
+// a)
+// process.argv[0] 
+
+// b)
+
+let eu = {
+    nome: "Ramon",
+    idade: 20,
+}
+
+console.log(`Olá, ${eu.nome}! Você tem ${eu.idade} anos.`)
+
+// c)
+
+eu = {
+    nome: "Ramon",
+    idade: 20,
+    idadeFutura: 27
+}
+
+console.log(`Olá, ${eu.nome}! Você tem ${eu.idade} anos. Em sete anos você terá ${eu.idadeFutura}.`)
+// Exercicio 2
+
+
+const operacao = () => {
+switch(operacao){
+	case "soma":
+		2 + 2
+		break;
+	case "subt":
+		2 - 2
+		break;
+    default:
+        return 
+}
+
+}
+process.argv[2]
+// Exercicio 3
+
+
+
+
+// Exercicio 4
