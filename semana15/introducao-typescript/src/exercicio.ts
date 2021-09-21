2924d9326e704c819c3650da68c036f5f599d025
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-09-21 19:56:29 -0300

    exercicio

---
+++ semana15/introducao-typescript/src/exercicio.ts
@@ -0,0 +1,41 @@
+// Exercicio 1
+// a)
+
+// R: TypeScript não permite a atribuição de outro tipo a variavel.
+
+// b)
+
+const meuNumero: number | string = 5
+
+// c)
+
+type pessoa = {
+	nome: string,
+    idade: number,
+	corFavorita: string
+}
+
+const pessoa = {
+    nome: "Ramon",
+    idade: 20,
+    corFavorita: "Preto"
+}
+
+// d)
+
+
+
+// Exercicio 2
+
+
+
+
+// Exercicio 3
+
+
+
+
+// Exercicio 4
+
+
+
