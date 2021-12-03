1f8bf72b48979cb8b2cf49452952c93bc80cb22d
Ramon Dias Ferreira <ramondf13@gmail.com>
2021-12-03 19:28:39 -0300

    Back-end

--- semana-cases/semana1/promobit/src/components/MovieRow.js
+++ semana-cases/semana1/promobit/src/components/MovieRow.js
@@ -1,10 +1,22 @@
 import React from "react";
 import './MovieRow.css'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
+
 
 export default ({title, items}) => {
     return(
         <div className='movies'>
             <h2>{title}</h2>
+
+        <div className='left'>
+        <NavigateBeforeIcon style={{fontSize: 50}}/>
+        </div>
+
+        <div className='right'>
+        <NavigateNextIcon style={{fontSize: 50}}/>
+        </div>
+
             <div className='movieListArea'>
                 <div className='movieList'>
                 {items.results.length > 0 && items.results.map((item, key) => (
